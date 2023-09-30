191692a86f0a6dd00c54d51c3de68c89ea89401e
Pariya Sayadi <pariyaSayadi@outlook.com>
2023-09-30 15:51:58 +0330

    Add project files.

---
+++ JWT-sample/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT_sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login(string username, string password)
+        {
+            // اعتبارسنجی نام کاربری و رمز عبور (اینجا می‌توانید منطق اعتبارسنجی و احراز هویت خود را اضافه کنید)
+            if (username == "YourUsername" && password == "YourPassword")
+            {
+                var token = GenerateJwtToken(username);
+                return Ok(new { token });
+            }
+
+            return Unauthorized();
+        }
+
+        [Authorize]
+        [HttpGet("protected")]
+        public IActionResult ProtectedResource()
+        {
+            return Ok("This is a protected resource.");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var jwtSecret = "MySuperSecretKey"; // همان کلید مخفی که در Program.cs قرار داده‌ایم
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(jwtSecret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ JWT-sample/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+// تنظیمات JWT
+var jwtSecret = "MySuperSecretKey"; // کلید مخفی را در اینجا قرار دهید
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret)),
+        };
+    });
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
+
